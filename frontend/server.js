1781b7d78d5af1c8c11f59974db94b0841e71d5e
Christopher Powroznik <chrispow96@gmail.com>
2019-09-24 17:07:42 -0700

    add public directory for build in frontend

--- frontend/server.js
+++ frontend/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = process.env.PORT ? process.env.PORT : 8080;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
