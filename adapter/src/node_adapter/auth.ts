8e5906fcc83a0602e4b9afc45800626187df073e
Christopher Powroznik <chrispow96@gmail.com>
2019-11-02 13:41:06 -0700

    fix exports of auth

--- adapter/src/node_adapter/auth.ts
+++ adapter/src/node_adapter/auth.ts
@@ -1,23 +1,27 @@
 import IAPIResponse from "../IAPIResponse";
 import {fullURLs} from "./urls";
+import axios from "axios";
 
-export default {
-    logout: async() : Promise<IAPIResponse> => {
-        try {
-            return {success: false}
-        } catch {
-            return {success: false}
-        }
-    },
-    authenticate: async() : Promise<IAPIResponse> => {
-        try {
-            const {status, data} = await axios.get(fullURLs.AUTHENTICATE);
-            return {success: status === 200 && data}
-        } catch {
-            return {success: false};
-        }
-    },
-    loginRedirectURL: (): string => {
-        return fullURLs.LOGIN;
-    }
+async function logout(): Promise<IAPIResponse> {
+	try {
+		return {success: false}
+	} catch {
+		return {success: false}
+	}
 }
+
+async function isAuthenticated(): Promise<IAPIResponse> {
+	try {
+		const {status, data} = await axios.get(fullURLs.AUTHENTICATE);
+		return {success: status === 200 && data}
+	} catch {
+		return {success: false};
+	}
+}
+
+function loginRedirectURL(): string {
+	return fullURLs.LOGIN;
+}
+
+export {logout, isAuthenticated, loginRedirectURL};
+
