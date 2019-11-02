0714d280b19a19cb8cf1ff6850e1ff4db3064bd3
Christopher Powroznik <chrispow96@gmail.com>
2019-11-02 13:16:26 -0700

    added isAuthenticated to exports

--- adapter/src/node_adapter/index.ts
+++ adapter/src/node_adapter/index.ts
@@ -2,13 +2,14 @@ import IAPIAdapter from "../IAPIAdapter";
 import placeholderAdapter from "../placeholder_adapter";
 import {urls, fullURLs} from "./urls";
 import health from "./health";
-import {loginRedirectURL, logout} from "./auth";
+import {loginRedirectURL, logout, isAuthenticated} from "./auth";
 import admin from "./admin";
 
 const adapter: IAPIAdapter = {
 	...placeholderAdapter,
 	loginRedirectURL,
 	logout,
+	isAuthenticated,
 	...admin,
 	health,
 	urls,
