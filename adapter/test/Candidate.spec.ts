2b5afc59032da654d05aed92e54d3020f4d05fda
cinders-p <askcinders@outlook.com>
2019-11-02 14:59:41 -0700

    update npm scripts for running adapter tests

--- adapter/test/Candidate.spec.ts
+++ adapter/test/Candidate.spec.ts
@@ -1,9 +1,52 @@
 import "mocha";
 import {expect} from "chai";
-import Adapter from "../src/IAPIAdapter";
 
-describe("Test Candidate Functionality", () => {
-    context("submitAvailability", async () => {
-        
-    })
+import {ICandidate} from "../dist/interfaces";
+import adapter from "../src/node_adapter";
+
+describe("Candidate Lifecycle Tests", () => {
+    let mockCandidate: ICandidate = {
+        email: "admin+tester@ph14solutions.onmicrosoft.com",
+        phoneNumber: "17781234567",
+        firstName: "Test",
+        lastName: "Doe"
+    };
+
+    context("createCandidate", () => {
+        it("should fail if candidate is missing an email", async () => {
+            const incompleteCandidate: ICandidate = {
+                firstName: "Test",
+                lastName: "Doe"
+            };
+            const {success, error} = await adapter.createCandidate(incompleteCandidate);
+            expect(success).to.be.false;
+            expect(error).to.not.be.undefined;
+        });
+
+        it("should succeed", async () => {
+            const incompleteCandidate: ICandidate = {
+                firstName: "Test",
+                lastName: "Doe"
+            };
+            const {success, data} = await adapter.createCandidate(incompleteCandidate);
+            expect(success).to.be.true;
+            expect(data.id).to.not.be.null;
+            mockCandidate = data;
+        });
+    });
+
+    context("updateCandidate", () => {
+
+    });
+
+    context("sendAvailabilityEmail", () => {
+
+    });
+
+    context("submitAvailability", () => {
+    });
+
+    context("deleteCandidate", () => {
+
+    });
 });
