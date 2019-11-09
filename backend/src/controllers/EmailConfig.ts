98daf79a927ded9146176a2ef0728a546dd7bb0e
ksyeo1010 <ksyeo1996@gmail.com>
2019-11-09 14:06:05 -0800

    changed header

--- backend/src/controllers/EmailConfig.ts
+++ backend/src/controllers/EmailConfig.ts
@@ -2,11 +2,16 @@ import {interfaces} from "adapter/dist";
 import {Config, ConfigKey} from "../Config";
 
 const createEmail = (candidate: interfaces.ICandidate) => {
-    let name = `${candidate.firstName || ""} ${candidate.lastName || ""},`.trim();
+    let header = '';
+    if (!candidate.firstName && !candidate.lastName) {
+        header = 'Hello,'
+    } else {
+        header = `Hello ${candidate.firstName || ""} ${candidate.lastName || ""},`
+    }
 
-    const html = `<h4>Hello ${name}</h4>
-                          <p>Please submit your availability by clicking the link below so we can schedule an interview.</p>
-                          <a href="${Config.getInstance().get(ConfigKey.frontendUrl)}/submit_availability/${candidate.id}">Link to availability</a>`
+    const html = `<h4>${header}</h4>
+                  <p>Please submit your availability by clicking the link below so we can schedule an interview.</p>
+                  <a href="${Config.getInstance().get(ConfigKey.frontendUrl)}/submit_availability/${candidate.id}">Link to availability</a>`
 
     return {
         message: {
