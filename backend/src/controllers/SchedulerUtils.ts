91e26b60d6011c5e6870eb81672f3b2dfad3ebb5
braxtonhall <braxtonjhall@gmail.com>
2019-11-21 16:09:02 -0800

    Start schedulerutils

--- backend/src/controllers/SchedulerUtils.ts
+++ backend/src/controllers/SchedulerUtils.ts
@@ -5,6 +5,12 @@ export interface IScheduleAvailabilities {
 	interviewers: {interviewer: interfaces.IInterviewer, availability: interfaces.IAvailability}[]
 }
 
+const took = (start, end) => Date.parse(end) - Date.parse(start);
+const slotLength = m => took(m.start as string, m.end as string);
+const sumAvailability: (a) => number = availability => availability.map(slotLength).reduce((m, n) => m + n, 0);
+const biggestSlot = availability => availability.map(slotLength).reduce((m, n) => m > n ? m : n, 0);
+const averageSlot = availability => sumAvailability(availability) / availability.length;
+
 export function concatenateMoments(availability: interfaces.IAvailability): interfaces.IAvailability {
 	if (availability.some(m => typeof m.start !== "string" || typeof m.end !== "string")) {
 		throw new Error("Cannot concatenate non-string Moments");
@@ -30,6 +36,25 @@ export function concatenateMoments(availability: interfaces.IAvailability): inte
 	return output;
 }
 
-function took(start: string, end: string): number {
-	return Date.parse(end) - Date.parse(start);
+function clipNonWorkingHours(availability: interfaces.IAvailability): interfaces.IAvailability {
+	// TODO trim off any availability times outside of working hours
+	return availability;
+}
+
+function findOverlappingTime(availA: interfaces.IAvailability, availB: interfaces.IAvailability): interfaces.IAvailability {
+	// TODO Given two availabilities, finds their overlap
+	return availA;
+}
+
+function findAverageOverlapSum(avail: interfaces.IAvailability, testAvails: interfaces.IAvailability[]): number {
+	return testAvails
+		.map(a => sumAvailability(findOverlappingTime(avail, a)))
+		.reduce((m, n) => m + n, 0) / testAvails.length;
+}
+
+function scoreRoom(room: interfaces.IAvailability, interviewers: interfaces.IAvailability[], numRooms: number): number {
+	// TODO THIS IS THE HEART OF THIS WHOLE ALGORITHM! THE NUMBERS ARE BETA AND SUBJECT TO CHANGE!
+	/**
+	 * Here are the things that I may want to include in a room score
+	 */
 }
