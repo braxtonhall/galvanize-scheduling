b157cec2a96f9a25c4d402076ecc0e14c10e4d8f
Christopher Powroznik <chrispow96@gmail.com>
2019-11-24 11:58:23 -0800

    authenticateAndLogout implemented

--- frontend/src/component/AuthenticatedRoute.tsx
+++ frontend/src/component/AuthenticatedRoute.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {Redirect, Route, RouteProps} from "react-router";
 import Context from "../services/Context";
 
 const AuthenticatedRoute: React.FC<RouteProps> = ({ component: Component, ...rest }) => {
 	const context = useContext(Context);
+	// useEffect(context.authenticateAndLogout, []);
+
 	return (
 		<Route
 			{...rest}
 			render={(props) => {
+
 				if (!context.token || context.token.length < 1) {
 					return <Redirect
 						to={{
@@ -17,6 +20,8 @@ const AuthenticatedRoute: React.FC<RouteProps> = ({ component: Component, ...res
 						}}
 					/>
 				}
+
+				context.authenticateAndLogout();
 
 				// @ts-ignore
 				return <Component {...props}/>
