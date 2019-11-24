e4a4700e370e6c8c04a3aae30b37b3fe7f422b5e
Christopher Powroznik <chrispow96@gmail.com>
2019-11-23 20:52:18 -0800

    removed old adapters

--- adapter/src/index.ts
+++ adapter/src/index.ts
@@ -1,12 +1,8 @@
 import nodeAdapter from "./node_adapter";
-import placeholderAdapter from "./placeholder_adapter";
-import localDataAdapter from "./local_data_adapter";
 import * as interfaces from "./interfaces"
 
 export {
-	placeholderAdapter,
 	nodeAdapter,
-	localDataAdapter,
 
 	interfaces,
 }
